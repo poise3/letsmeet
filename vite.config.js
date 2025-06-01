0c647eb37ad83accaaf084372df6a690b5962f53
tehmiw <tehmingwei1@gmail.com>
2025-06-02 04:54:33 +0800

    Fix externalization of react-big-calendar in Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(), react()],
+  build: {
+    rollupOptions: {
+      external: ['react-big-calendar'], // Mark react-big-calendar as external
+    },
+  },
 })
